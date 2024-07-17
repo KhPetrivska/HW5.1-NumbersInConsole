0e0762ae9fe9cd992b4701556cc89ef7e24c80f0
Kristina <kristin.petrivska@gmail.com>
2024-07-17 00:30:48 -0600

    add variables

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict'
+const smalestNum = 20;
+const biggestNum = 30;
+const step = 0.5;
 let numbers = '';
-for (let i = 20; i <= 30; i = i + 0.5) {
+for (let i = smalestNum; i <= biggestNum; i = i + step) {
    numbers += i + ' ';
 }
 console.log(numbers);
